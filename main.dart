a29b462ea18e36eabd5214d99b46bbd9c9a9d363
yenitaputri <137504376+yenitaputri@users.noreply.github.com>
2023-10-13 17:59:01 +0700

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,15 @@
+import 'package:assassment1/personal_form.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: PersonalForm(),
+    );
+  }
+}
